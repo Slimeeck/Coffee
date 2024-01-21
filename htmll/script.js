6caf9bb9b9c71c4a87b4341e2da8968d27ed9382
Slime <127672051+Slimeeck@users.noreply.github.com>
2024-01-21 15:06:32 +0200

    Update script.js

--- htmll/script.js
+++ htmll/script.js
@@ -1,22 +1,17 @@
-// JavaScript-код для вашої сторінки
 document.querySelector('nav').addEventListener('click', function (e) {
     if (e.target.tagName === 'A') {
         e.preventDefault();
 
-        // Отримайте ідентифікатор анкора з посилання
         var targetId = e.target.getAttribute('href').substring(1);
 
-        // Знайдіть всі розділи на сторінці
         var sections = document.querySelectorAll('section');
 
-        // Закрийте всі розділи, окрім вибраного
         sections.forEach(function (section) {
             if (section.id !== targetId) {
                 section.classList.add('hidden');
             }
         });
 
-        // Знайдіть вибраний розділ і розкрийте його
         var targetSection = document.getElementById(targetId);
 
         if (targetSection) {
@@ -26,14 +21,12 @@ document.querySelector('nav').addEventListener('click', function (e) {
             }, 100);
         }
 
-        // Приховати картинки у розділі "Меню," якщо не обрано "Меню"
         if (targetId !== 'menu') {
             var menuImages = document.querySelectorAll('#menu img');
             menuImages.forEach(function (image) {
                 image.style.display = 'none';
             });
         } else {
-            // Показати картинки у розділі "Меню"
             var menuImages = document.querySelectorAll('#menu img');
             menuImages.forEach(function (image) {
                 image.style.display = 'block';
